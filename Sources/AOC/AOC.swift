d0520fa9aaa0958e3629532c7e4cd0e386e0de8d
Jessy Catterwaul <jessy@razeware.com>
2022-11-30 16:54:29 -0500

    String.input & .lines

--- Sources/AOC/AOC.swift
+++ Sources/AOC/AOC.swift
@@ -1,1 +1,18 @@
+import struct Foundation.URL
 
+public extension String {
+  static func input(forTestFile file: String = #file) throws -> Self {
+    try .init(
+      contentsOf:
+        URL(fileURLWithPath: file)
+        .deletingPathExtension()
+        .deletingLastPathComponent()
+        .appendingPathComponent("Input")
+        .appendingPathExtension("md")
+    )
+  }
+
+  static func lines(forTestFile file: String = #file) throws -> [Substring] {
+    try input(forTestFile: file).split(whereSeparator: \.isNewline)
+  }
+}
