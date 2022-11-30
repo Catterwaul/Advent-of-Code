8175bce2ed43578cdbea4cbdeb1684e500ccc58a
Jessy Catterwaul <jessy@razeware.com>
2022-11-30 14:40:53 -0500

    2021.1

--- Sources/2021/1/2021.1.swift
+++ Sources/2021/1/2021.1.swift
@@ -1,1 +1,12 @@
+import Algorithms
+import HM
 
+public extension Sequence where Element: Comparable {
+  var increaseCount: Int { adjacentPairs().lazy.filter(<).count }
+}
+
+public extension Collection where Element: AdditiveArithmetic & Comparable {
+  var slidingWindowIncreaseCount: Int {
+    windows(ofCount: 3).lazy.map(\.sum!).increaseCount
+  }
+}
