ccbd54afe84d0c32a4606b92190a91fb90b7c8b2
Jessy Catterwaul <jessy@razeware.com>
2022-11-30 18:22:14 -0500

    2021.2

--- Sources/2021/2/2021.2.swift
+++ Sources/2021/2/2021.2.swift
@@ -1,1 +1,53 @@
+public final class Submarine {
+  public struct Command {
+    public enum Axis {
+      case forward, down
+    }
 
+    public let axis: Axis
+    public let units: Int
+  }
+
+  public init(
+    position: Int = 0,
+    depth: Int = 0
+  ) {
+    self.position = position
+    self.depth = depth
+  }
+
+  public var position: Int
+  public var depth: Int
+}
+
+public extension Submarine {
+  func follow(course: some Sequence<(some StringProtocol, some StringProtocol)>) {
+    for command in course.map(Command.init) {
+      switch command.axis {
+      case .forward:
+        position += command.units
+      case .down:
+        depth += command.units
+      }
+    }
+  }
+
+  func follow(course: some Sequence<[some StringProtocol]>) {
+    follow(course: course.map { ($0[0], $0[1]) })
+  }
+}
+
+public extension Submarine.Command {
+  init(direction: some StringProtocol, units: some StringProtocol) {
+    switch direction {
+    case "forward":
+      self.init(axis: .forward, units: .init(units)!)
+    case "down":
+      self.init(axis: .down, units: .init(units)!)
+    case "up":
+      self.init(axis: .down, units: -.init(units)!)
+    default:
+      fatalError()
+    }
+  }
+}
--- Tests/2021/2/TestCase.swift
+++ Tests/2021/2/TestCase.swift
@@ -1,8 +1,30 @@
+import AOC
 import AOC_2021_2
 import XCTest
 
+import Foundation
+
 final class TestCase: XCTestCase {
   func test() {
-    
+    let input = """
+      forward 5
+      down 5
+      forward 8
+      up 3
+      down 8
+      forward 2
+      """
+    let submarine = Submarine()
+    submarine.follow(course: input.linesSplitBySpaces)
+    XCTAssertEqual(submarine.position, 15)
+    XCTAssertEqual(submarine.depth, 10)
+  }
+
+  func test_answers() throws {
+    let submarine = Submarine()
+    submarine.follow(course: try String.input().linesSplitBySpaces)
+    XCTAssertEqual(submarine.position, 1817)
+    XCTAssertEqual(submarine.depth, 1072)
+    XCTAssertEqual(submarine.position * submarine.depth, 1947824)
   }
 }
