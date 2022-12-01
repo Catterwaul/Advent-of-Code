640bf0d86bdf7280459396c9914b4cd276912ce9
Jessy Catterwaul <jessy@razeware.com>
2022-12-01 14:55:08 -0500

    2021.3.1

--- Tests/2021/3/TestCase.swift
+++ Tests/2021/3/TestCase.swift
@@ -6,6 +6,28 @@ import Foundation
 
 final class TestCase: XCTestCase {
   func test() {
+    let input = """
+      00100
+      11110
+      10110
+      10111
+      10101
+      01111
+      00111
+      11100
+      10000
+      11001
+      00010
+      01010
+      """
+      .lines
+    let rates = input.rates
+    XCTAssertEqual(rates.gamma, 22)
+    XCTAssertEqual(rates.epsilon, 9)
+  }
 
+  func test_answer() throws {
+    let rates = try String.input().lines.rates
+    XCTAssertEqual(rates.gamma * rates.epsilon, 2967914)
   }
 }
