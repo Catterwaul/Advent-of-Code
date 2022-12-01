ce736dde5fa37c3fc00d767c8e4f53a9211daa54
Jessy Catterwaul <jessy@razeware.com>
2022-12-01 07:43:53 -0500

    2022.1.2

--- Sources/2022/1/2022.1.swift
+++ Sources/2022/1/2022.1.swift
@@ -2,10 +2,11 @@ import HM
 import Algorithms
 
 public extension Sequence where Element: StringProtocol {
-  var maxCalories: Int {
-    split(whereSeparator: \.isEmpty)
+  func maxCalories(count: Int = 1) -> Int {
+    lazy.split(whereSeparator: \.isEmpty)
       .map(\.calorieSum)
-      .max()!
+      .max(count: count)
+      .sum!
   }
   
   private var calorieSum: Int {
