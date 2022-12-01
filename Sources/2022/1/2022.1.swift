efa86bb3be4db7682d5a201e9649ae327485c7b7
Jessy Catterwaul <jessy@razeware.com>
2022-12-01 08:12:52 -0500

    Document

--- Sources/2022/1/2022.1.swift
+++ Sources/2022/1/2022.1.swift
@@ -2,6 +2,10 @@ import HM
 import Algorithms
 
 public extension Sequence where Element: StringProtocol {
+  /// - Precondition: Each element is
+  /// 1. an integer
+  /// or
+  /// 2. a blank line, representing the split between two elves.
   func maxCalories(count: Int = 1) -> Int {
     lazy.split(whereSeparator: \.isEmpty)
       .map(\.calorieSum)
@@ -9,6 +13,7 @@ public extension Sequence where Element: StringProtocol {
       .sum!
   }
   
+  /// - Precondition: Each element is an integer.
   private var calorieSum: Int {
     lazy.map { Int($0)! }.sum!
   }
