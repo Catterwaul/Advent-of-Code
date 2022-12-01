79b70a10479e5ccfc054bfbea73981fc8792a195
Jessy Catterwaul <jessy@razeware.com>
2022-12-01 14:55:08 -0500

    2021.3.1

--- Sources/2021/3/2021.3.swift
+++ Sources/2021/3/2021.3.swift
@@ -1,1 +1,14 @@
+import HM
 
+public extension Collection where Element: StringProtocol {
+  var rates: (gamma: UInt, epsilon: UInt) {
+    let gamma = UInt(
+      bitPattern:
+        transposed.map {
+          let onesAreMostCommon = $0.count { $0 == "1" } > count / 2
+          return onesAreMostCommon
+        }
+    )!
+    return (gamma, (~gamma)[mask: ..<first!.count])
+  }
+}
