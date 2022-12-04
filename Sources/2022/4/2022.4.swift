7f538854a95d01d0f12e7e299e352cce8bfffe56
Jessy Catterwaul <jessy@razeware.com>
2022-12-04 10:41:24 -0500

    2022.4.1

--- Sources/2022/4/2022.4.swift
+++ Sources/2022/4/2022.4.swift
@@ -1,1 +1,19 @@
+import HM
 
+public func overlapCount(
+  _ assignmentPairIDs: some Sequence<[[some StringProtocol]]>
+) -> Int {
+  assignmentPairIDs.lazy.count {
+    let ranges = $0.map(ClosedRange.init)
+    return [(0, 1), (1, 0)].contains {
+      ranges[$0].contains(ranges[$1])
+    }
+  }
+}
+
+private extension ClosedRange<Int> {
+  init(_ roomIDStrings: [some StringProtocol]) {
+    let ids = roomIDStrings.map { Int($0)! }
+    self = ids[0]...ids[1]
+  }
+}
--- Tests/2022/4/TestCase.swift
+++ Tests/2022/4/TestCase.swift
@@ -4,10 +4,31 @@ import XCTest
 
 final class TestCase: XCTestCase {
   func test() {
-    
+    let input = """
+      2-4,6-8
+      2-3,4-5
+      5-7,7-9
+      2-8,3-7
+      6-6,4-6
+      2-6,4-8
+
+      """
+      .assignmentPairIDs()
+    XCTAssertEqual(overlapCount(input), 2)
   }
 
-  func test_answers() {
+  func test_answers() throws {
+    XCTAssertEqual(
+      overlapCount(try String.input().assignmentPairIDs()), 507
+    )
+  }
+}
 
+private extension String {
+  func assignmentPairIDs() -> [[[some StringProtocol]]] {
+    lines.map {
+      $0.split(separator: ",")
+        .map { $0.split(separator: "-") }
+    }
   }
 }
