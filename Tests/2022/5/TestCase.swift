5b31cca8fd975726a0101db003441aa627ffac59
Jessy Catterwaul <jessy@razeware.com>
2022-12-05 12:06:05 -0500

    2022.5.1

--- Tests/2022/5/TestCase.swift
+++ Tests/2022/5/TestCase.swift
@@ -5,11 +5,32 @@ import XCTest
 final class TestCase: XCTestCase {
   func test() {
     let input = """
+          [D]   \n\
+      [N] [C]   \n\
+      [Z] [M] [P]
+       1   2   3
 
-    """
+      move 1 from 2 to 1
+      move 3 from 1 to 3
+      move 2 from 2 to 1
+      move 1 from 1 to 2
+
+      """
+      .lines
+    XCTAssertEqual(
+      Array.crateStacks(drawing: input.split(whereSeparator: \.isEmpty)[0]),
+      [ ["Z", "N"],
+        ["M", "C", "D"],
+        ["P"]
+      ]
+    )
+    XCTAssertEqual(topCrates(input: input), "CMZ")
   }
 
-  func test_answers() {
-
+  func test_answers() throws {
+    XCTAssertEqual(
+      try topCrates(input: String.input().lines),
+      "GRTSWNJHH"
+    )
   }
 }
