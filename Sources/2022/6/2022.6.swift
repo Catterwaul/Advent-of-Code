dbd54d578c4bde357e2fac9f6ab1bcbf13b927fa
Jessy Catterwaul <jessy@razeware.com>
2022-12-06 12:43:38 -0500

    2022.6.1

--- Sources/2022/6/2022.6.swift
+++ Sources/2022/6/2022.6.swift
@@ -1,1 +1,11 @@
+import Algorithms
+import HM
 
+public extension String {
+  var indexOfLastCharacterInMarker: Int {
+    let markerCount = 4
+    return windows(ofCount: markerCount).enumerated()
+      .first(where: \.element.containsOnlyUniqueElements)!
+      .offset + markerCount
+  }
+}
--- Tests/2022/6/TestCase.swift
+++ Tests/2022/6/TestCase.swift
@@ -4,12 +4,14 @@ import XCTest
 
 final class TestCase: XCTestCase {
   func test() {
-    let input = """
-
-    """
+    XCTAssertEqual("mjqjpqmgbljsphdztnvjfqwrcgsmlb".indexOfLastCharacterInMarker, 7)
+    XCTAssertEqual("bvwbjplbgvbhsrlpgdmjqwftvncz".indexOfLastCharacterInMarker, 5)
+    XCTAssertEqual("nppdvjthqldpwncqszvftbrmjlhg".indexOfLastCharacterInMarker, 6)
+    XCTAssertEqual("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".indexOfLastCharacterInMarker, 10)
+    XCTAssertEqual("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".indexOfLastCharacterInMarker, 11)
   }
 
-  func test_answers() {
-
+  func test_answers() throws {
+    XCTAssertEqual(try String.input().indexOfLastCharacterInMarker, 1987)
   }
 }
