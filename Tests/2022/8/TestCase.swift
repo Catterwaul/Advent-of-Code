fa17830f92e76e84060c972863e6fd0e8f1aad8b
Jessy Catterwaul <jessy@razeware.com>
2022-12-08 15:03:21 -0500

    2022.8.1

--- Tests/2022/8/TestCase.swift
+++ Tests/2022/8/TestCase.swift
@@ -5,11 +5,18 @@ import XCTest
 final class TestCase: XCTestCase {
   func test() {
     let input = """
-
-    """
+      30373
+      25512
+      65332
+      33549
+      35390
+      
+      """
+      .lines
+    XCTAssertEqual(visibleTreeCount(input), 21)
   }
 
-  func test_answers() {
-
+  func test_answers() throws {
+    XCTAssertEqual(try visibleTreeCount(String.input().lines), 21)
   }
 }
