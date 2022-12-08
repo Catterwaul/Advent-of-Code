581fa30d67ce7257a78e169e237f4e66fc55ea29
Jessy Catterwaul <jessy@razeware.com>
2022-12-07 23:11:43 -0500

    2022.7.1

--- Sources/2022/7/2022.7.swift
+++ Sources/2022/7/2022.7.swift
@@ -1,1 +1,70 @@
+import HM
 
+public final class Directory {
+  private init(
+    parentDirectory: Directory?,
+    contents: some Sequence<some StringProtocol>
+  ) {
+    self.parentDirectory = parentDirectory
+    subdirectories = []
+    fileSizeSum =
+      contents.lazy.compactMap {
+        Int($0.split(whereSeparator: \.isWhitespace)[0])
+      }
+      .sum ?? 0
+    parentDirectory?.subdirectories.append(self)
+    parentDirectory?.fileSizeSum += fileSizeSum
+  }
+
+  private unowned let parentDirectory: Directory?
+  private let subdirectories: [Directory]
+
+  private var fileSizeSum: Int {
+    didSet {
+      parentDirectory?.fileSizeSum += (fileSizeSum - oldValue)
+    }
+  }
+}
+
+public extension Directory {
+  convenience init(input: some Collection<some StringProtocol>) {
+    var iterator = input.chunked { Command($1) == nil }.makeIterator()
+    self.init(
+      parentDirectory: nil,
+      contents: iterator.next()!
+    )
+
+    var workingDirectory = self
+    IteratorSequence(iterator).forEach {
+      var iterator = $0.makeIterator()
+      switch Command(iterator.next()!)! {
+      case .moveIn:
+        workingDirectory = .init(
+          parentDirectory: workingDirectory,
+          contents: IteratorSequence(iterator)
+        )
+      case .moveOut:
+        workingDirectory = workingDirectory.parentDirectory!
+      }
+    }
+  }
+
+  var smallSubdirectoryFileSizeSum: Int {
+    let bug =
+      recursive(self, \.subdirectories).lazy
+        .map(\.fileSizeSum)
+        .filter { $0 < 100_000 }
+        .sum
+    return bug!
+  }
+}
+
+private enum Command {
+  case moveIn, moveOut
+
+  init?(_ line: some StringProtocol) {
+    let iterator = AnyIterator(line.makeIterator())
+    guard iterator.starts(with: "$ cd") else { return nil }
+    self = iterator.starts(with: "..") ? .moveOut : .moveIn
+  }
+}
--- Tests/2022/7/TestCase.swift
+++ Tests/2022/7/TestCase.swift
@@ -5,11 +5,39 @@ import XCTest
 final class TestCase: XCTestCase {
   func test() {
     let input = """
-
-    """
+      $ cd /
+      $ ls
+      dir a
+      14848514 b.txt
+      8504156 c.dat
+      dir d
+      $ cd a
+      $ ls
+      dir e
+      29116 f
+      2557 g
+      62596 h.lst
+      $ cd e
+      $ ls
+      584 i
+      $ cd ..
+      $ cd ..
+      $ cd d
+      $ ls
+      4060174 j
+      8033020 d.log
+      5626152 d.ext
+      7214296 k
+      
+      """
+      .lines
+    XCTAssertEqual(Directory(input: input).smallSubdirectoryFileSizeSum, 95437)
   }
 
-  func test_answers() {
-
+  func test_answers() throws {
+    XCTAssertEqual(
+      try Directory(input: String.input().lines).smallSubdirectoryFileSizeSum,
+      1427048
+    )
   }
 }
