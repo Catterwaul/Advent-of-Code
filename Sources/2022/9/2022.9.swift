77a5430eeb54eddfbd4baa30c93f36ffedf69993
Jessy Catterwaul <jessy@razeware.com>
2022-12-09 14:00:09 -0500

    2022.9.1

--- Sources/2022/9/2022.9.swift
+++ Sources/2022/9/2022.9.swift
@@ -1,1 +1,46 @@
+import simd
 
+public struct Tail {
+  private var position: Vector {
+    didSet { positionsVisited.insert(position) }
+  }
+
+  public private(set) var positionsVisited: Set<SIMD2<Int32>>
+}
+
+public extension Tail {
+  /// - Parameter headMotions: Input lines split by spaces.
+  init(headMotions: some Sequence<[some StringProtocol]>) {
+    var headPosition = Vector.zero {
+      didSet {
+        let difference = headPosition &- position
+        let clamped = clamp(difference, min: -1, max: 1)
+        if difference != clamped { position &+= clamped }
+      }
+    }
+
+    self.init(position: headPosition, positionsVisited: [headPosition])
+
+    headMotions.lazy.flatMap {
+      repeatElement(.init($0[0]), count: .init($0[1])!)
+    }.forEach {
+      headPosition &+= $0
+    }
+  }
+}
+
+// MARK: -
+
+private typealias Vector = SIMD2<Int32>
+
+private extension Vector {
+  init(_ input: some StringProtocol) {
+    switch input {
+    case "U": self = .init(0, 1)
+    case "D": self = .init(0, -1)
+    case "L": self = .init(-1, 0)
+    case "R": self = .init(1, 0)
+    default: fatalError()
+    }
+  }
+}
--- Tests/2022/9/TestCase.swift
+++ Tests/2022/9/TestCase.swift
@@ -5,11 +5,25 @@ import XCTest
 final class TestCase: XCTestCase {
   func test() {
     let input = """
+      R 4
+      U 4
+      L 3
+      D 1
+      R 4
+      D 1
+      L 5
+      R 2
 
-    """
+      """
+      .linesSplitBySpaces
+    XCTAssertEqual(Tail(headMotions: input).positionsVisited.count, 13)
   }
 
-  func test_answers() {
-
+  func test_answers() throws {
+    XCTAssertEqual(
+      Tail(headMotions: try String.input().linesSplitBySpaces)
+        .positionsVisited.count,
+      6197
+    )
   }
 }
