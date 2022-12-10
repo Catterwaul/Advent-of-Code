edd33220a5783101fd2dfa1b37aecaf6416d0790
Jessy Catterwaul <jessy@razeware.com>
2022-12-10 15:01:49 -0500

    2022.10

--- Sources/2022/10/2022.10.swift
+++ Sources/2022/10/2022.10.swift
@@ -1,1 +1,54 @@
+import Algorithms
+import HM
 
+public func signalStrengthSum(
+  cycles: some Sequence<Int>,
+  _ lines: some Sequence<some Sequence<some StringProtocol>>
+) -> Int {
+  var iterator = cycles.makeIterator()
+  var index = iterator.next()
+  return xRegisterSequence(lines).enumerated().lazy
+    .filter { index == $0.offset + 1 }
+    .compactMap { x in
+      defer { index = iterator.next() }
+      return index.map { x.element * $0 }
+    }
+    .sum!
+}
+
+public func signalStrength(
+  atCycle cycleIndex: Int,
+  _ lines: some Sequence<some Sequence<some StringProtocol>>
+) -> Int {
+  let xRegister = xRegisterSequence(lines).enumerated().dropFirst(cycleIndex - 1).first!
+  return xRegister.element * (xRegister.offset + 1)
+}
+
+/// - Parameter lines: Input lines split by spaces.
+func xRegisterSequence(
+  _ lines: some Sequence<some Sequence<some StringProtocol>>
+) -> some Sequence<Int> {
+  chain(
+    1,
+    sequence(
+      state: (
+        xRegister: 1,
+        v: Int?.none,
+        iterator: lines.makeIterator()
+      )
+    ) {
+      if let v = $0.v {
+        $0.xRegister += v
+        $0.v = nil
+      } else if let next = $0.iterator.next() {
+        if let v = (next.last.flatMap { Int($0) }) {
+          $0.v = v
+        }
+      } else {
+        return nil as Int?
+      }
+
+      return $0.xRegister
+    }
+  )
+}
--- Tests/2022/10/TestCase.swift
+++ Tests/2022/10/TestCase.swift
@@ -5,11 +5,178 @@ import XCTest
 final class TestCase: XCTestCase {
   func test() {
     let input = """
+      addx 15
+      addx -11
+      addx 6
+      addx -3
+      addx 5
+      addx -1
+      addx -8
+      addx 13
+      addx 4
+      noop
+      addx -1
+      addx 5
+      addx -1
+      addx 5
+      addx -1
+      addx 5
+      addx -1
+      addx 5
+      addx -1
+      addx -35
+      addx 1
+      addx 24
+      addx -19
+      addx 1
+      addx 16
+      addx -11
+      noop
+      noop
+      addx 21
+      addx -15
+      noop
+      noop
+      addx -3
+      addx 9
+      addx 1
+      addx -3
+      addx 8
+      addx 1
+      addx 5
+      noop
+      noop
+      noop
+      noop
+      noop
+      addx -36
+      noop
+      addx 1
+      addx 7
+      noop
+      noop
+      noop
+      addx 2
+      addx 6
+      noop
+      noop
+      noop
+      noop
+      noop
+      addx 1
+      noop
+      noop
+      addx 7
+      addx 1
+      noop
+      addx -13
+      addx 13
+      addx 7
+      noop
+      addx 1
+      addx -33
+      noop
+      noop
+      noop
+      addx 2
+      noop
+      noop
+      noop
+      addx 8
+      noop
+      addx -1
+      addx 2
+      addx 1
+      noop
+      addx 17
+      addx -9
+      addx 1
+      addx 1
+      addx -3
+      addx 11
+      noop
+      noop
+      addx 1
+      noop
+      addx 1
+      noop
+      noop
+      addx -13
+      addx -19
+      addx 1
+      addx 3
+      addx 26
+      addx -30
+      addx 12
+      addx -1
+      addx 3
+      addx 1
+      noop
+      noop
+      noop
+      addx -9
+      addx 18
+      addx 1
+      addx 2
+      noop
+      noop
+      addx 9
+      noop
+      noop
+      noop
+      addx -1
+      addx 2
+      addx -37
+      addx 1
+      addx 3
+      noop
+      addx 15
+      addx -21
+      addx 22
+      addx -6
+      addx 1
+      noop
+      addx 2
+      addx 1
+      noop
+      addx -10
+      noop
+      noop
+      addx 20
+      addx 1
+      addx 2
+      addx 2
+      addx -6
+      addx -11
+      noop
+      noop
+      noop
 
-    """
+      """
+      .linesSplitBySpaces
+    XCTAssertEqual(signalStrength(atCycle: 20, input), 420)
+    XCTAssertEqual(signalStrength(atCycle: 60, input), 1140)
+    XCTAssertEqual(signalStrength(atCycle: 100, input), 1800)
+    XCTAssertEqual(signalStrength(atCycle: 140, input), 2940)
+    XCTAssertEqual(signalStrength(atCycle: 180, input), 2880)
+    XCTAssertEqual(signalStrength(atCycle: 220, input), 3960)
+
+    XCTAssertEqual(
+      signalStrengthSum(
+        cycles: [20, 60, 100, 140, 180, 220],
+        input
+      ),
+      13140
+    )
   }
 
-  func test_answers() {
-
+  func test_answers() throws {
+    XCTAssertEqual(
+      signalStrengthSum(
+        cycles: [20, 60, 100, 140, 180, 220],
+        try String.input().linesSplitBySpaces
+      ),
+      15680
+    )
   }
 }
