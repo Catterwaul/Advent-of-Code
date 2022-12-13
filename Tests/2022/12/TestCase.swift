e6f00098c6f6f4ce01382783115242a722be279e
Jessy Catterwaul <jessy@razeware.com>
2022-12-12 20:07:33 -0500

    12

--- Tests/2022/12/TestCase.swift
+++ Tests/2022/12/TestCase.swift
@@ -5,11 +5,21 @@ import XCTest
 final class TestCase: XCTestCase {
   func test() {
     let input = """
+      Sabqponm
+      abcryxxl
+      accszExk
+      acctuvwj
+      abdefghi
 
       """
+      .lines
+
+    XCTAssertEqual(stepCountFromStart(input), 31)
+    XCTAssertEqual(stepCountFromLowestElevation(input), 29)
   }
 
-  func test_answers() {
-
+  func test_answers() throws {
+    XCTAssertEqual(try stepCountFromStart(String.input().lines), 490)
+    XCTAssertEqual(try stepCountFromLowestElevation(String.input().lines), 488)
   }
 }
