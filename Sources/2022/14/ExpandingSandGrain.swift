c4fff0144f00bd6363c6d7dc536b9a255cdecd0a
Jessy Catterwaul <jessy@razeware.com>
2022-12-14 11:39:51 -0500

    Expanding Grid

---
+++ Sources/2022/14/ExpandingSandGrain.swift
@@ -0,0 +1,54 @@
+import HM
+
+struct ExpandingSandGrain {
+  struct Error: Swift.Error { }
+
+  init(grid: ExpandingGrid) {
+    self.grid = grid
+    position = [500, 0] &- grid.origin
+  }
+
+  private let grid: ExpandingGrid
+  private(set) var position: Vector
+
+  mutating func fall() throws {
+    let p = position
+    while true {
+      do {
+        try fallOneStep()
+      } catch let error as Matrix<Grid.Element>.IndexingError {
+        throw error
+      } catch {
+        if p == position, grid.matrix[p] == .sand { throw Error() }
+        break
+      }
+    }
+  }
+
+  private mutating func fallOneStep() throws {
+    position = try [[0, 1] as SIMD2<Int>, [-1, 1], [1, 1]]
+      .lazy.compactMap { [position] (offset: SIMD2) in
+        var position = position &+ offset
+
+        do {
+          switch try grid.matrix[validating: position] {
+          case .air: return position
+          case .rock, .sand: return nil
+          }
+        } catch {
+          guard position.y >= 0 else { throw error }
+          guard position.y < grid.matrix.size.y - 1 else { return nil }
+
+          if position.x < 0 {
+            grid.matrix.columns.insert(grid.emptyColumn, at: 0)
+            grid.origin.x -= 1
+            position.x += 1
+          } else {
+            grid.matrix.columns.append(grid.emptyColumn)
+          }
+
+          return position
+        }
+      }.first.unwrapped
+  }
+}
