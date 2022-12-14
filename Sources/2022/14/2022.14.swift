c3d9c0a17dde641e88c567dc8ca11d48318bd61d
Jessy Catterwaul <jessy@razeware.com>
2022-12-14 05:28:49 -0500

    Add rocks

--- Sources/2022/14/2022.14.swift
+++ Sources/2022/14/2022.14.swift
@@ -8,24 +8,43 @@ public func parse(_ input: some Sequence<some StringProtocol>) -> SIMD2<Int> {
     }
   }
 
-  let rockExtrema = lines.lazy.flatMap { $0 }.reduce(
-    (min: [.max, .max] as SIMD2, max: [.min, .min] as SIMD2)
-  ) {
-    ( [min($0.min.x, $1.x), min($0.min.y, $1.y)],
-      [max($0.max.x, $1.x), max($0.max.y, $1.y)]
-    )
-  }
+  let grid = Grid(lines)
 
-  let size = rockExtrema.max &- rockExtrema.min &+ .one
-  let grid = Matrix(
-    columns: sequence().prefix(size.x).lazy.map {
-      sequence().prefix(size.y).lazy.map { GridElement.air }
-    }
-  )
-
-  return grid.size
+  return grid.matrix.size
 }
 
-enum GridElement {
-  case air, rock, sand
+struct Grid {
+  enum Element {
+    case air, rock, sand
+  }
+
+  var matrix: Matrix<Element>
+  let origin: SIMD2<Int>
+}
+
+extension Grid {
+  init(_ rockPaths: [[SIMD2<Int>]]) {
+    let matrixProperties = rockPaths.lazy.flatMap { $0 }.reduce(
+      (origin: [.max, .max] as SIMD2, max: [.min, .min] as SIMD2)
+    ) {
+      ( [min($0.origin.x, $1.x), min($0.origin.y, $1.y)],
+        [max($0.max.x, $1.x), max($0.max.y, $1.y)]
+      )
+    }
+
+    origin = matrixProperties.origin
+
+    let size = matrixProperties.max &- origin &+ .one
+    matrix = .init(
+      columns: sequence().prefix(size.x).lazy.map {
+        sequence().prefix(size.y).lazy.map { .air }
+      }
+    )
+
+    rockPaths.lazy.flatMap { path in
+      path.lazy.map { position in position &- matrixProperties.origin }
+    }.adjacentPairs().lazy.flatMap(...).forEach {
+      matrix.columns[$0.x][$0.y] = .rock
+    }
+  }
 }
