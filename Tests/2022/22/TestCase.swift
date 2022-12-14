2e9e778dd29c7f00800125cd4b43b21ebb61666c
Jessy Catterwaul <jessy@razeware.com>
2022-12-13 23:41:05 -0500

    Add the rest of 2022's files

---
+++ Tests/2022/22/TestCase.swift
@@ -0,0 +1,15 @@
+import AOC
+import AOC_2022_22
+import XCTest
+
+final class TestCase: XCTestCase {
+  func test() {
+    let input = """
+
+      """
+  }
+
+  func test_answers() {
+
+  }
+}
---
+++ Tests/2022/24/TestCase.swift
@@ -0,0 +1,15 @@
+import AOC
+import AOC_2022_24
+import XCTest
+
+final class TestCase: XCTestCase {
+  func test() {
+    let input = """
+
+      """
+  }
+
+  func test_answers() {
+
+  }
+}
