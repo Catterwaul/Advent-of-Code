6a45db2571d5576b607413c709d0b4823f2f17fe
Jessy Catterwaul <jessy@razeware.com>
2022-12-14 04:38:15 -0500

    Create grid of air

--- Sources/2022/14/2022.14.swift
+++ Sources/2022/14/2022.14.swift
@@ -1,1 +1,31 @@
+import Algorithms
+import HM
 
+public func parse(_ input: some Sequence<some StringProtocol>) -> SIMD2<Int> {
+  let lines = input.map {
+    $0.split(whereSeparator: Set(" ->").contains).map {
+      SIMD2($0.split(separator: ",").lazy.map { Int($0)! })
+    }
+  }
+
+  let rockExtrema = lines.lazy.flatMap { $0 }.reduce(
+    (min: [.max, .max] as SIMD2, max: [.min, .min] as SIMD2)
+  ) {
+    ( [min($0.min.x, $1.x), min($0.min.y, $1.y)],
+      [max($0.max.x, $1.x), max($0.max.y, $1.y)]
+    )
+  }
+
+  let size = rockExtrema.max &- rockExtrema.min &+ .one
+  let grid = Matrix(
+    columns: sequence().prefix(size.x).lazy.map {
+      sequence().prefix(size.y).lazy.map { GridElement.air }
+    }
+  )
+
+  return grid.size
+}
+
+enum GridElement {
+  case air, rock, sand
+}
--- Tests/2022/14/TestCase.swift
+++ Tests/2022/14/TestCase.swift
@@ -5,11 +5,15 @@ import XCTest
 final class TestCase: XCTestCase {
   func test() {
     let input = """
+      498,4 -> 498,6 -> 496,6
+      503,4 -> 502,4 -> 502,9 -> 494,9
 
       """
+      .lines
+    XCTAssertEqual(parse(input), [10, 6])
   }
 
-  func test_answers() {
-
+  func test_answers() throws {
+    XCTAssertEqual(parse(try String.input().lines), [73, 159])
   }
 }
