a843521df6baf5ea37788f37935fee61faa20b4f
Jessy Catterwaul <jessy@razeware.com>
2022-12-17 22:14:31 -0500

    13.1

--- Sources/2022/13/2022.13.swift
+++ Sources/2022/13/2022.13.swift
@@ -1,4 +1,7 @@
 import Algorithms
+import HM
+
+typealias Packet = ValueArrayNest<Int>
 
 public func rightOrderedIndexSum(
   _ distressSignal: some Collection<some StringProtocol>
@@ -6,57 +9,68 @@ public func rightOrderedIndexSum(
   let x = distressSignal.split(whereSeparator: \.isEmpty)
     .lazy.flatMap { $0 }
     .lazy.map(ValueArrayNest.init)
-//    .first!
 
-
-  let xa = Array(x)
-
-
-  return 0
+  return x.chunks(ofCount: 2).count { $0.first! < $0.last! }
 }
 
 
-extension ValueArrayNest<Int> {
+extension Packet {
   init(_ packet: some StringProtocol) {
-    typealias ArrayElement = Reference<ReferenceArrayNest<Element>>
-    var lists: [ArrayElement] = [.array()]
-    var lastList: ArrayElement { lists.last! }
+    typealias ArrayNestReference = Reference<ReferenceArrayNest<Element>>
 
-    packet
-      .dropFirst() // so `lastList` doesn't need to be optional
-      .dropLast() // so `lists[0]` doesn't get removed
-      .split(separator: ",")
-      .lazy.flatMap {
-        $0.chunked { $0.isNumber && $1.isNumber }
-      }
-      .forEach { string in
-        switch string {
-        case "[":
-          let list = ArrayElement.array()
-          try! lastList.wrappedValue.append(list)
-          lists.append(list) // push it onto the stack
-        case "]":
-          lists.removeLast() // pop off the top of the stack
-        default:
-          try! lastList.wrappedValue.append(.element(Int(string)!))
+    self.init(
+      packet
+        .dropFirst() // so `lastList` doesn't need to be optional
+        .dropLast() // so `lists[0]` doesn't get removed
+        .split(separator: ",")
+        .lazy.flatMap {
+          $0.chunked { $0.isNumber && $1.isNumber }
         }
-      }
+        .reduce(into: [ArrayNestReference.array()]) { lists, string in
+          var lastList: ArrayNestReference { lists.last! }
 
-    self.init(lastList.wrappedValue)
+          switch string {
+          case "[":
+            let list = ArrayNestReference.array()
+            try! lastList.wrappedValue.append(list)
+            lists.append(list) // push it onto the stack
+          case "]":
+            lists.removeLast() // pop off the top of the stack
+          default:
+            try! lastList.wrappedValue.append(.element(Int(string)!))
+          }
+        }[0].wrappedValue
+    )
   }
 }
 
-//extension PacketData {
-//  static func < (data0: PacketData, data1: PacketData) -> Bool {
-//    switch (data0, data1) {
-//    case (.integer(let int0), .integer(let int1)):
-//      return  int0 < int1
-//    case (.list(let list0), .list(let list1)):
-//      return false
-//    case (.integer(let int0), .list):
-//      return .list([.init(int0)]) < data1
-//    case (.list, .integer(let int1)):
-//      return data0 < .list([.init(int1)])
-//    }
-//  }
-//}
+extension Packet {
+  static func < (packet0: Self, packet1: Self) -> Bool {
+    func array(_ element: Element) -> Self {
+      .array([.element(element)])
+    }
+
+    switch (packet0, packet1) {
+    case (.element(let int0), .element(let int1)):
+      return  int0 < int1
+    case (.array(let list0), .array(let list1)):
+      return AnySequence(zip: (list0, list1))
+        .prefixThroughFirst { $0 == nil || $1 == nil }
+        .allSatisfy {
+          switch $0 {
+          case (let packet0?, let packet1?):
+            return packet0 < packet1
+          case (nil, .some):
+            return true
+          case (.some, nil):
+            return false
+          case (nil, nil): fatalError()
+          }
+        }
+    case (.element(let int0), .array):
+      return array(int0) < packet1
+    case (.array, .element(let int1)):
+      return packet0 < array(int1)
+    }
+  }
+}
