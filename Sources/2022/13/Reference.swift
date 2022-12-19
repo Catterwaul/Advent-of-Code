1a7e77bca968bf34d42bd99d393913f7e61215f3
Jessy Catterwaul <jessy@razeware.com>
2022-12-19 01:56:40 -0500

    Move ArrayNest to HM

--- Sources/2022/13/Reference.swift
+++
@@ -1,9 +0,0 @@
-/// Adds reference semantics to a value type.
-@propertyWrapper public final class Reference<Value> {
-  public var wrappedValue: Value
-  public var projectedValue: Reference { self }
-
-  public init(wrappedValue: Value) {
-    self.wrappedValue = wrappedValue
-  }
-}
