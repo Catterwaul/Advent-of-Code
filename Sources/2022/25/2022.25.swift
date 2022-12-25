3468b76fa3555691666524646559e6e4d887190d
Jessy Catterwaul <jessy@razeware.com>
2022-12-25 11:42:10 -0500

    25.1

--- Sources/2022/25/2022.25.swift
+++ Sources/2022/25/2022.25.swift
@@ -1,1 +1,52 @@
+import HM
 
+public func snafuNumber(
+  _ input: some Sequence<some StringProtocol>
+) -> String {
+  .init(snafuNumber: snafuNumber(input))
+}
+
+public func snafuNumber(
+  _ input: some Sequence<some StringProtocol>
+) -> Int {
+  input.lazy.map {
+    $0.reduce(0) { reduction, next in
+      reduction * 5 + {
+        switch next {
+        case "-": return -1
+        case "=": return -2
+        default: return next.wholeNumberValue!
+        }
+      } ()
+    }
+  }.sum!
+}
+
+public extension String {
+  init(snafuNumber: Int) {
+    self = sequence(state: snafuNumber) { quotientPlusCarry in
+      guard quotientPlusCarry > 0 else { return nil }
+
+      let division = quotientPlusCarry.quotientAndRemainder(dividingBy: 5)
+      let character: String
+      let carry: Int
+
+      switch division.remainder {
+      case 4:
+        character = "-"
+        carry = 1
+      case 3:
+        character = "="
+        carry = 1
+      case let remainder:
+        character = String(remainder)
+        carry = 0
+      }
+
+      quotientPlusCarry = division.quotient + carry
+      return character
+    }
+    .reversed()
+    .joined()
+  }
+}
