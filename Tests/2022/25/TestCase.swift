fe540317220610a1999059ef9831872db3436147
Jessy Catterwaul <jessy@razeware.com>
2022-12-25 11:42:10 -0500

    25.1

--- Tests/2022/25/TestCase.swift
+++ Tests/2022/25/TestCase.swift
@@ -5,11 +5,29 @@ import XCTest
 final class TestCase: XCTestCase {
   func test() {
     let input = """
-
+      1=-0-2
+      12111
+      2=0=
+      21
+      2=01
+      111
+      20012
+      112
+      1=-1=
+      1-12
+      12
+      1=
+      122
+      
       """
+      .lines
+
+    XCTAssertEqual(snafuNumber(input), 4890)
+    XCTAssertEqual(snafuNumber(input), "2=-1=0")
   }
 
-  func test_answers() {
-
+  func test_answers() throws {
+    XCTAssertEqual(snafuNumber(try String.input().lines), 35951702021395)
+    XCTAssertEqual(snafuNumber(try String.input().lines), "2-21=02=1-121-2-11-0")
   }
 }
